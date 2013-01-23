ab8af3d89d07e15237660e74f87d8db613b54b69
Russel <ruslan.malogulko@google.com>
2013-01-23 16:06:26 +0200

    initial commit

---
+++ testldap.py
@@ -0,0 +1,18 @@
+import ldap
+
+server = 'ldap://172.22.64.41:389'
+username = r'nc.local\rmalogulko'
+password= 'lp5023PL1'
+base_dn='ou=Technical,dc=nc,dc=local'
+
+l = ldap.initialize(server)
+l.protocol_version = 3
+try :
+	l.simple_bind_s(username,password)
+	searchFilter = "cn=*"
+	retrieveAttributes = ['cn']
+	results = l.search_s(base_dn,ldap.SCOPE_SUBTREE,searchFilter, retrieveAttributes)
+	print results
+except ldap.LDAPError:
+	print "Sorry"
+#print s
